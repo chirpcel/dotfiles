646b61ea64475e09ed003d6a4fdc07071db9d087
chirpcel <git@mkersten.net>
2024-09-24 20:27:12 +0200

    chore(install): update install script 🚀

--- install.sh
+++ install.sh
@@ -1,36 +1,29 @@
 #!/bin/zsh
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "detected macOS"
+    export XDG_CONFIG_HOME=~/Library/Preferences/
 
     echo "apply vscode configuration"
     ln -s ${PWD}/code/settings.json ./settings.json
-
-    echo "apply atuin configuration"
-    ln -s ${PWD}/atuin ~/Library/Preferences/atuin
-
-    echo "apply alacritty configuration"
-    ln -s ${PWD}/alacritty ~/Library/Preferences/alacritty
-
-    echo "apply zsh configuration"
-    ln -s ${PWD}/zsh ~/Library/Preferences/zsh
-    echo "source ~/Library/Preferences/zsh/source.zsh" >> ~/.zshrc
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "detected linux"
-
+    export XDG_CONFIG_HOME=~/.config/
     echo "apply vscode configuration"
-    mkdir -p ~/.config/code
-    ln -s ${PWD}/code/settings.json ~/.config/code/settings.json
-
-    echo "apply atuin configuration"
-    ln -s ${PWD}/atuin ~/.config/atuin
-
-    echo "apply alacritty configuration"
-    ln -s ${PWD}/alacritty ~/.config/alacritty
-
-    echo "apply zsh configuration"
-    ln -s ${PWD}/zsh ~/.config/zsh
-    echo "source ~/.config/zsh/source.zsh" >> ~/.zshrc
+    ln -s ${PWD}/code/settings.json ${XDG_CONFIG_HOME}/code/settings.json
 fi
 
+echo "apply atuin configuration"
+ln -s ${PWD}/atuin ${XDG_CONFIG_HOME}/atuin
+
+echo "apply alacritty configuration"
+ln -s ${PWD}/alacritty ${XDG_CONFIG_HOME}/alacritty
+
+echo "apply zsh configuration"
+ln -s ${PWD}/zsh ${XDG_CONFIG_HOME}/zsh
+echo "source ${XDG_CONFIG_HOME}/zsh/source.zsh" >> ~/.zshrc
+
 echo "apply docker configuration"
-ln -s ${PWD}/docker ~/.docker
+ln -s ${PWD}/docker ${XDG_CONFIG_HOME}/docker
+
+echo "apply tmux configuration"
+ln -s ${PWD}/tmux ${XDG_CONFIG_HOME}/tmux
