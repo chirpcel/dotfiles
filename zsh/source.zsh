ad3c63ca070fb8e12a5f98dcf49a1a180b7d2a31
chirpcel <git@mkersten.net>
2024-09-28 23:59:49 +0200

    fix(zsh): fix xdg definitions

--- zsh/source.zsh
+++ zsh/source.zsh
@@ -1,9 +1,9 @@
 # xdg
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  export XDG_CONFIG_HOME=$HOME/Library/Preferences/
-  export XDG_CACHE_HOME=$HOME/Library/Caches/
-  export XDG_DATA_HOME=$HOME/Library/Application\ Support/
-  export XDG_STATE_HOME=$HOME/Library/Saved\ Application\ State/
+  export XDG_CONFIG_HOME=$HOME/Library/Preferences
+  export XDG_CACHE_HOME=$HOME/Library/Caches
+  export XDG_DATA_HOME=$HOME/Library/Application\ Support
+  export XDG_STATE_HOME=$HOME/Library/Saved\ Application\ State
   export DOCKER_CONFIG=$HOME/Library/Preferences/docker
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   export DOCKER_CONFIG=$HOME/.config/docker
