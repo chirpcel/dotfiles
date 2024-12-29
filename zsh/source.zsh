cc9e489c4b3b177cc0ad042ffe15d82dd8a20892
chirpcel <git@mkersten.net>
2024-12-29 03:33:28 +0100

    chore(zellij): auto start zellij on shell in macOS Terminal.app 🚸

--- zsh/source.zsh
+++ zsh/source.zsh
@@ -1,4 +1,12 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
+export ZELLIJ_CONFIG_DIR=~/Library/Preferences/zellij
+export ZELLIJ_AUTO_EXIT=true
+if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then
+  eval "$(zellij setup --generate-auto-start zsh)"
+  alias clear="clear && pfetch"
+fi
+
+export STARSHIP_CONFIG=~/Library/Preferences/starship/starship.toml
 eval "$(starship init zsh)"
 
 export DOCKER_CONFIG=${XDG_CONFIG_HOME}/docker
@@ -10,11 +18,8 @@ export PF_COL2=8
 export PF_PAD1=2
 export PF_PAD2=3
 export PF_PAD3=5
-if [[ "$ATTY" == "true"* ]]; then
-  alias clear="clear && pfetch"
-fi
-clear
 
+clear
 dir_path=$(dirname "$0")
 for script in $(ls $dir_path/source.d/*.zsh | sort); do
   source $script
