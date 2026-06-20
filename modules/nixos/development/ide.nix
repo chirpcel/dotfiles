a51599e7a46318721533e6e04342d6062a92d87e
chirpcel <git@m.krstn.io>
2026-06-20 15:02:33 +0200

    chore(development): add rust-stable,node24 sdk

--- modules/nixos/development/ide.nix
+++ modules/nixos/development/ide.nix
@@ -3,10 +3,13 @@
     services.flatpak = {
       packages = [
         "dev.zed.Zed"
+        "org.freedesktop.Sdk.Extension.rust-stable/x86_64/25.08"
+        "org.freedesktop.Sdk.Extension.node24/x86_64/25.08"
       ];
       overrides."dev.zed.Zed" = {
         Environment = {
           ZED_FLATPAK_NO_ESCAPE = "1";
+          FLATPAK_ENABLE_SDK_EXT = "rust-stable,node24";
         };
       };
     };
